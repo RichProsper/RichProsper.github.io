582fc8375a283cf4f7ac5c90fd695103f3b5c65e
RichProsper <r.stp.p@hotmail.com>
2021-02-06 19:49:25 -0400

    Updates to banner

--- src/header.js
+++ src/header.js
@@ -1,15 +1,19 @@
 const header = document.getElementById('header')
+const menuClose = document.getElementsByClassName('menu-close')[0]
 
 window.addEventListener('scroll', () => {
     // Add/remove sticky, depending on test conditional, scrollY greater than 0
     header.classList.toggle('sticky', window.scrollY > 0)
 })
 
-const toggle = () => header.classList.toggle('active')
+const toggle = () => {
+    header.classList.toggle('active')
+    menuClose.classList.toggle('close')
+}
 
 const lis = header.children[1].children
 for (let i = 0; i < lis.length; i++) {
     lis[i].children[0].addEventListener('click', toggle)
 }
 
-document.getElementsByClassName('menu-close')[0].addEventListener('click', toggle)
+menuClose.addEventListener('click', toggle)
