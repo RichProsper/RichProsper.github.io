15443325c1d7b53bbd72ebd00616f0b5602c46c6
RichProsper <r.stp.p@hotmail.com>
2021-02-07 17:24:13 -0400

    Updates to Header

--- src/header.js
+++ src/header.js
@@ -5,12 +5,22 @@ window.addEventListener('scroll', () => {
     // Add/remove sticky, depending on test conditional, scrollY greater than 0
     header.classList.toggle('sticky', window.scrollY > 0)
 })
+window.addEventListener('resize', () => {
+    // If menu was opened when screen size was small close it when screen is resized
+    // to bigger. This avoids some styling issues.
+    if (window.innerWidth >= 993 && header.classList.contains('active')) {
+        header.classList.remove('active')
+        menuClose.classList.remove('close')
+    }
+})
 
 if (window.scrollY > 0) header.classList.add('sticky')
 
 const toggle = () => {
-    if (window.innerWidth < 993) header.classList.toggle('active')
-    menuClose.classList.toggle('close')
+    if (window.innerWidth < 993) {
+        header.classList.toggle('active')
+        menuClose.classList.toggle('close')
+    }
 }
 
 const lis = header.children[1].children
