865d2bafea9392f8a7298ded472d0b7d38422195
RichProsper <r.stp.p@hotmail.com>
2021-02-07 16:28:15 -0400

    Added Portfolio Coming Soon

---
+++ src/portfolio.js
@@ -0,0 +1,26 @@
+const countDownDate = new Date("Feb 28, 2021 23:59:59").getTime()
+const countDownElem = document.getElementById("countdown")
+
+const countDown = () => {
+    const now = new Date().getTime()
+    const distance = countDownDate - now
+    
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24))
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000)
+    
+    countDownElem.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s "
+    return distance
+}
+
+const d = countDown()
+
+const countDownInterval = setInterval(function() {
+    const distance = countDown()
+    
+    if (distance < 0) {
+        clearInterval(countDownInterval)
+        countDownElem.innerHTML = "EXPIRED"
+    }
+}, 1000)
