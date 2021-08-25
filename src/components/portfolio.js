2ba14593cb7b0c9664c36beef5d838c6f0bbc8ef
RichProsper <r.stp.p@hotmail.com>
2021-08-25 17:00:53 -0400

    Setting up portfolio...

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,28 +1,5 @@
 export default () => {
-    const countDownDate = new Date("Dec 31, 2021 23:59:59").getTime()
-    const countDownElem = document.getElementById("countdown")
+    const carousel = document.querySelector('[data-carousel]')
 
-    const countDown = () => {
-        const now = new Date().getTime()
-        const distance = countDownDate - now
-        
-        const days = Math.floor(distance / (1000 * 60 * 60 * 24))
-        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-        const seconds = Math.floor((distance % (1000 * 60)) / 1000)
-        
-        countDownElem.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s "
-        return distance
-    }
-
-    const d = countDown()
-
-    const countDownInterval = setInterval(function() {
-        const distance = countDown()
-        
-        if (distance < 0) {
-            clearInterval(countDownInterval)
-            countDownElem.innerHTML = "EXPIRED"
-        }
-    }, 1000)
+    
 }
