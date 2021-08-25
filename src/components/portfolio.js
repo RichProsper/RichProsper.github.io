f52775bf8b0fd11e5e95875687b9b561014b71e0
RichProsper <r.stp.p@hotmail.com>
2021-08-25 18:10:09 -0400

    Setting up carousel

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,5 +1,9 @@
 export default () => {
     const carousel = document.querySelector('[data-carousel]')
+    const items = carousel.querySelector('.items')
+    const total = carousel.querySelector('.total')
 
-    
+    total.innerHTML = items.children.length
+
+    console.log(total)
 }
