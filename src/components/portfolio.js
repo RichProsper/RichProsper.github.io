fcd2a2fb750e73daeba58b6f361ad8b37658d1d5
RichProsper <r.stp.p@hotmail.com>
2021-08-25 19:27:42 -0400

    Carousel completed.

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,9 +1,47 @@
 export default () => {
     const carousel = document.querySelector('[data-carousel]')
     const items = carousel.querySelector('.items')
-    const total = carousel.querySelector('.total')
+    const total = items.children.length
+    const totalSpan = carousel.querySelector('.total')
+    const current = carousel.querySelector('.current')
+    const prevBtn = carousel.querySelector('.prev')
+    const nextBtn = carousel.querySelector('.next')
 
-    total.innerHTML = items.children.length
+    totalSpan.innerHTML = total
 
-    console.log(total)
+    prevBtn.addEventListener('click', () => {
+        if (total === 1) return
+
+        let i = +current.innerHTML
+        items.children[i - 1].classList.add('hidden')
+        
+        if (i === 1) {
+            i = total
+            current.innerHTML = i
+        }
+        else {
+            i--
+            current.innerHTML = i
+        }
+
+        items.children[i - 1].classList.remove('hidden')
+    })
+
+    nextBtn.addEventListener('click', () => {
+        if (total === 1) return
+        
+        let i = +current.innerHTML
+        items.children[i - 1].classList.add('hidden')
+        
+        if (i === total) {
+            i = 1
+            current.innerHTML = i
+        }
+        else {
+            i++
+            current.innerHTML = i
+        }
+
+        items.children[i - 1].classList.remove('hidden')
+    })
 }
