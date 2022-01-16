0849c87ff5a5f24c4dd2c216df6530f676c38104
Rich Prosper <47926284+RichProsper@users.noreply.github.com>
2022-01-15 20:35:12 -0400

    Updates to Switch

--- projects/rui/form-inputs/switch/rui-switch.js
+++ projects/rui/form-inputs/switch/rui-switch.js
@@ -11,7 +11,27 @@ export default ({ labelText, attrs = {} }) => {
             const Base = document.createElement('span')
             Base.setAttribute('data-switch-base', '')
 
+                // Input - input[type="checkbox"]
+                const Checkbox = document.createElement('input')
+                Checkbox.type = 'checkbox'
+                Checkbox.addEventListener('change', function() {
+                    if (this.checked) Base.classList.add('checked')
+                    else Base.classList.remove('checked')
+                })
+
+                // Thumb - span
+                const Thumb = document.createElement('span')
+                Thumb.setAttribute('data-switch-thumb', '')
+
+            Base.appendChild(Checkbox)
+            Base.appendChild(Thumb)
+
+            // Track - span
+            const Track = document.createElement('span')
+            Track.setAttribute('data-switch-track', '')
+
         SwitchWrapper.appendChild(Base)
+        SwitchWrapper.appendChild(Track)
 
     SwitchContainer.appendChild(SwitchWrapper)
     
