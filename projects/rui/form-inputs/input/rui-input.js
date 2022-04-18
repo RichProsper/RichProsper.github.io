8fd25ccf61af35fe6eab4b9d27f011dba54ad7ae
Rich Prosper <47926284+RichProsper@users.noreply.github.com>
2022-04-18 16:32:00 -0400

    Input type file (1)

--- projects/rui/form-inputs/input/rui-input.js
+++ projects/rui/form-inputs/input/rui-input.js
@@ -1,14 +1,14 @@
 export default ({ attrs = {}, evts = {} }) => {
-    const InputContainer = document.createElement('div')
-    InputContainer.setAttribute('data-input-container', '')
+    const DivContainer = document.createElement('div')
+    DivContainer.setAttribute('data-input-container', '')
 
         const Input = document.createElement('input')
-
+    
         // Set attributes
         for (const attr in attrs) Input.setAttribute(attr, attrs[attr])
 
-        Input.addEventListener('focus', () => InputContainer.classList.add('focused'))
-        Input.addEventListener('blur', () => InputContainer.classList.remove('focused'))
+        Input.addEventListener('focus', () => DivContainer.classList.add('focused'))
+        Input.addEventListener('blur', () => DivContainer.classList.remove('focused'))
 
         // Set events
         for (const evt in evts) {
@@ -18,13 +18,15 @@ export default ({ attrs = {}, evts = {} }) => {
             else Input.addEventListener(evt, evts[evt])
         }
 
-    InputContainer.appendChild(Input)
+    DivContainer.appendChild(Input)
     
     if (attrs?.placeholder) {
         const Span = document.createElement('span')
         Span.textContent = attrs?.placeholder
-        InputContainer.appendChild(Span)
+        DivContainer.appendChild(Span)
     }
 
-    return InputContainer
+    if (attrs?.type === 'file') DivContainer.setAttribute('data-file', '')
+
+    return DivContainer
 }
