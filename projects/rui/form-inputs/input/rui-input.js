78c7b55852dca5515e987560caf977d6ef93d05d
Rich Prosper <47926284+RichProsper@users.noreply.github.com>
2022-04-19 23:14:45 -0400

    Input type file (3)

--- projects/rui/form-inputs/input/rui-input.js
+++ projects/rui/form-inputs/input/rui-input.js
@@ -1,49 +1,97 @@
 export default ({ attrs = {}, evts = {} }) => {
-    const Container = { el: {} }
-    const Input = document.createElement('input')
-    
-    // Set attributes
-    for (const attr in attrs) Input.setAttribute(attr, attrs[attr])
-
-    Input.addEventListener('focus', () => Container.el.classList.add('focused'))
-    Input.addEventListener('blur', () => Container.el.classList.remove('focused'))
-
-    // Set events
-    for (const evt in evts) {
-        if ( Array.isArray(evts[evt]) ) {
-            for (const e of evts[evt]) Input.addEventListener(evt, e)
-        }
-        else Input.addEventListener(evt, evts[evt])
-    }
-
     switch (attrs?.type) {
         case 'file': {
-            Container.el = document.createElement('label')
+            const InputContainer = document.createElement('label')
+            InputContainer.setAttribute('data-input-container', '')
+            InputContainer.title = 'Choose a file...'
 
-            const Icon = document.createElement('i')
-            Icon.className = 'fas fa-upload'
+                const Input = document.createElement('input')
+        
+                // Set attributes
+                for (const attr in attrs) Input.setAttribute(attr, attrs[attr])
 
-            const Span = document.createElement('span')
-            Span.textContent = ' Choose a file...'
+                Input.addEventListener('focus', () => InputContainer.classList.add('focused'))
+                Input.addEventListener('blur', () => InputContainer.classList.remove('focused'))
 
-            Container.el.appendChild(Input)
-            Container.el.appendChild(Icon)
-            Container.el.appendChild(Span)
-            break
+                // Set events
+                for (const evt in evts) {
+                    if ( Array.isArray(evts[evt]) ) {
+                        for (const e of evts[evt]) Input.addEventListener(evt, e)
+                    }
+                    else Input.addEventListener(evt, evts[evt])
+                }
+
+                const Div = document.createElement('div')
+
+                    const Icon = document.createElement('i')
+                    Icon.className = 'fas fa-upload'
+
+                    const Span = document.createElement('span')
+                    Span.textContent = ' Choose a file...'
+
+                Div.appendChild(Icon)
+                Div.appendChild(Span)
+
+                Input.addEventListener('change', e => {
+                    const files = e.target.files
+                    switch (files.length) {
+                        case 0: {
+                            Span.textContent = ' Choose a file...'
+                            InputContainer.title = 'Choose a file...'
+                            break
+                        }
+                        case 1: {
+                            Span.textContent = ` ${files[0].name}`
+                            InputContainer.title = files[0].name
+                            break
+                        }
+                        default: {
+                            if (files.length < 9) {
+                                Span.textContent = ` ${files.length} files selected`
+                                InputContainer.title = `${files.length} files selected`
+                            }
+                            else {
+                                Span.textContent = ' 9+ files selected'
+                                InputContainer.title = '9+ files selected'
+                            }
+                        }
+                    }
+                })
+
+            InputContainer.appendChild(Input)
+            InputContainer.appendChild(Div)
+            
+            return InputContainer
         }
         default: {
-            Container.el = document.createElement('div')
-            Container.el.appendChild(Input)
+            const InputContainer = document.createElement('div')
+            InputContainer.setAttribute('data-input-container', '')
+
+                const Input = document.createElement('input')
+    
+                // Set attributes
+                for (const attr in attrs) Input.setAttribute(attr, attrs[attr])
+
+                Input.addEventListener('focus', () => InputContainer.classList.add('focused'))
+                Input.addEventListener('blur', () => InputContainer.classList.remove('focused'))
+
+                // Set events
+                for (const evt in evts) {
+                    if ( Array.isArray(evts[evt]) ) {
+                        for (const e of evts[evt]) Input.addEventListener(evt, e)
+                    }
+                    else Input.addEventListener(evt, evts[evt])
+                }
+
+            InputContainer.appendChild(Input)
     
             if (attrs?.placeholder) {
                 const Span = document.createElement('span')
                 Span.textContent = attrs?.placeholder
-                Container.el.appendChild(Span)
+                InputContainer.appendChild(Span)
             }
+
+            return InputContainer
         }
     }
-
-    Container.el.setAttribute('data-input-container', '')
-
-    return Container.el
 }
