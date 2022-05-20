b745b59c512562b9b5eb668b30b58697f5c28dd9
Rich Prosper <47926284+RichProsper@users.noreply.github.com>
2022-05-19 22:56:07 -0400

    Building modal component (4)

--- projects/rwc/modal/rwc-modal.js
+++ projects/rwc/modal/rwc-modal.js
@@ -1,4 +1,8 @@
 class RWC_Modal extends HTMLElement {
+    static get observedAttributes() {
+        return ['modal_outline_color'];
+    }
+
     constructor() {
         super()
         this.init()
@@ -9,17 +13,18 @@ class RWC_Modal extends HTMLElement {
         this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true))
 
         this.Modal = this.shadowRoot.querySelector('.modal')
-        this.Style = this.shadowRoot.querySelector('.style')
-        this.defaultModalOutlineColor = 'hsl(var(--hs-1), 63%)'
+        this.Style = this.shadowRoot.querySelector('style')
     }
 
     getTemplate() {
-        const style = `
-            <style class="style">
-                *,*::before,*::after{margin:0;padding:0}.modal{--hs-1: 0, 0%;--white-1: hsl(var(--hs-1), 33%);--white-2: hsla(var(--hs-1), 10%, .75);--white-3: hsl(var(--hs-1), 10%);--white-4: hsl(var(--hs-1), 5%);--white-5: hsl(var(--hs-1), 53%);--modal-outline-color: [[modal-outline-color]];display:none;z-index:1;position:fixed;top:0;left:0;width:100%;height:100vh;background-color:var(--white-2);padding:8em 4em;-webkit-box-sizing:border-box;box-sizing:border-box}.modal.open{display:block}.modal *,.modal *::before,.modal *::after{-webkit-box-sizing:border-box;box-sizing:border-box}.modal .content{background-color:var(--white-3);border-radius:1.5rem;border:0.2rem solid var(--modal-outline-color)}.modal .content .header{position:relative;height:4rem;border-bottom:0.2rem solid var(--white-1)}.modal .content .header h3{font-size:2rem;line-height:4rem;padding:0 1.5rem;white-space:nowrap;overflow:hidden;text-overflow:ellipsis}.modal .content .header .close{position:absolute;top:-1.5rem;right:-1.5rem;width:3rem;height:3rem;border-radius:50%;background-color:var(--white-3);border:.2rem solid;border-color:var(--modal-outline-color) var(--modal-outline-color) var(--modal-outline-color) transparent;-webkit-transform:rotate(-45deg);transform:rotate(-45deg);cursor:pointer;color:inherit;font-weight:300;font-size:3rem;line-height:0;-webkit-transition:.2s;transition:.2s}.modal .content .header .close span{display:block;-webkit-transform:rotate(45deg);transform:rotate(45deg)}.modal .content .header .close:hover,.modal .content .header .close:focus{color:var(--modal-outline-color)}.modal .content .body{padding-top:1.2rem;padding-bottom:1.5rem}.modal .content .body .body-content{display:block;max-height:calc(100vh - (2 * 8em) - 4rem);padding:0 1.5rem;overflow:auto}@media only screen and (min-width: 600px) and (min-height: 600px){.modal{padding:4em 8em}.modal .content .body .body-content{max-height:calc(100vh - (2 * 4em) - 4rem)}}.modal ::-webkit-scrollbar{width:15px;height:15px}.modal ::-webkit-scrollbar-track,.modal ::-webkit-scrollbar-corner{background:var(--white-4)}.modal ::-webkit-scrollbar-thumb{background:var(--white-5);border-radius:9px;border:3px solid var(--white-4)}.modal ::-webkit-scrollbar-thumb:hover{background:var(--white-5)}
-            </style>
+        this.defaultModalOutlineColor = 'hsl(var(--hs-1), 63%)'
+        this.css = `
+            *,*::before,*::after{margin:0;padding:0}.modal{--hs-1: 0, 0%;--white-1: hsl(var(--hs-1), 33%);--white-2: hsla(var(--hs-1), 10%, .75);--white-3: hsl(var(--hs-1), 10%);--white-4: hsl(var(--hs-1), 5%);--white-5: hsl(var(--hs-1), 53%);--modal-outline-color: [[modal_outline_color]];display:none;z-index:1;position:fixed;top:0;left:0;width:100%;height:100vh;background-color:var(--white-2);padding:8em 4em;-webkit-box-sizing:border-box;box-sizing:border-box}.modal.open{display:block}.modal *,.modal *::before,.modal *::after{-webkit-box-sizing:border-box;box-sizing:border-box}.modal .content{background-color:var(--white-3);border-radius:1.5rem;border:0.2rem solid var(--modal-outline-color)}.modal .content .header{position:relative;height:4rem;border-bottom:0.2rem solid var(--white-1)}.modal .content .header h3{font-size:2rem;line-height:4rem;padding:0 1.5rem;white-space:nowrap;overflow:hidden;text-overflow:ellipsis}.modal .content .header .close{position:absolute;top:-1.5rem;right:-1.5rem;width:3rem;height:3rem;border-radius:50%;background-color:var(--white-3);border:.2rem solid;border-color:var(--modal-outline-color) var(--modal-outline-color) var(--modal-outline-color) transparent;-webkit-transform:rotate(-45deg);transform:rotate(-45deg);cursor:pointer;color:inherit;font-weight:300;font-size:3rem;line-height:0;-webkit-transition:.2s;transition:.2s}.modal .content .header .close span{display:block;-webkit-transform:rotate(45deg);transform:rotate(45deg)}.modal .content .header .close:hover,.modal .content .header .close:focus{color:var(--modal-outline-color)}.modal .content .body{padding-top:1.2rem;padding-bottom:1.5rem}.modal .content .body .body-content{display:block;max-height:calc(100vh - (2 * 8em) - 4rem);padding:0 1.5rem;overflow:auto}@media only screen and (min-width: 600px) and (min-height: 600px){.modal{padding:4em 8em}.modal .content .body .body-content{max-height:calc(100vh - (2 * 4em) - 4rem)}}.modal ::-webkit-scrollbar{width:15px;height:15px}.modal ::-webkit-scrollbar-track,.modal ::-webkit-scrollbar-corner{background:var(--white-4)}.modal ::-webkit-scrollbar-thumb{background:var(--white-5);border-radius:9px;border:3px solid var(--white-4)}.modal ::-webkit-scrollbar-thumb:hover{background:var(--white-5)}
         `
-        const html = `
+        const template = document.createElement('template')
+        template.innerHTML = `
+            <style>${this.css}</style>
+
             <div class="modal" aria-modal="true" role="dialog">
                 <div class="content">
                     <div class="header">
@@ -36,9 +41,7 @@ class RWC_Modal extends HTMLElement {
                 </div>
             </div>
         `
-        const template = document.createElement('template')
-        template.innerHTML = style + html
-        
+
         return template
     }
 
@@ -58,7 +61,11 @@ class RWC_Modal extends HTMLElement {
             })
         }
 
-        this.Style.innerHTML = this.Style.innerHTML.replace('[[modal-outline-color]]', this.getAttribute('modal_outline_color') || this.defaultModalOutlineColor)
+        this.Style.innerHTML = this.css.replace('[[modal_outline_color]]', this.getAttribute('modal_outline_color') || this.defaultModalOutlineColor)
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        this.Style.innerHTML = this.css.replace('[[modal_outline_color]]', newValue)
     }
 }
 
