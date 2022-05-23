511748382f83c9fe4344441bba3a1821a4c3ef3f
Rich Prosper <47926284+RichProsper@users.noreply.github.com>
2022-05-23 18:58:18 -0400

    Added a custom web component

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import Header from './components/header.js'
 import Portfolio from './components/portfolio.js'
-import ToTop from './components/to-top.js'
 
 (() => {
     Header()
     Portfolio()
-    ToTop()
 })()
