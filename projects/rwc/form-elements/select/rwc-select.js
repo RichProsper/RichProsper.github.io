1c71dbb7abcfcc0590c7feb69e3afd1fd363761d
Rich Prosper <47926284+RichProsper@users.noreply.github.com>
2022-08-13 13:11:25 -0400

    Select (7)

--- projects/rwc/form-elements/select/rwc-select.js
+++ projects/rwc/form-elements/select/rwc-select.js
@@ -47,14 +47,47 @@ class RWC_Select extends HTMLElement {
 
     checkValidity  = () => this.internals_.checkValidity()
     reportValidity = () => this.internals_.reportValidity()
-    setFormValue   = () => this.internals_.setFormValue(this.value)
+    setFormValue   = () => {
+        if (!this.multiple){
+            this.internals_.setFormValue(this.value)
+            return
+        }
+
+        if (!this.name) return
+        const selectedOptions = []
+
+        for (const option of this.Select.querySelectorAll('option')) {
+            if (option.selected && !option.disabled) selectedOptions.push(option)
+        }
+        if (selectedOptions.length === 0) return
+
+        const formData = new FormData()
+
+        for (const selectedOption of selectedOptions) {
+            const value = selectedOption.getAttribute('value')
+
+            if (value) {
+                formData.append(this.name, value)
+                continue
+            }
+
+            if (selectedOption.hasAttribute('value')) {
+                formData.append(this.name, '')
+                continue
+            }
+
+            formData.append(this.name, selectedOption.textContent)
+        }
+
+        this.internals_.setFormValue(formData)
+    }
 
     /**
      * This is called when the 'disabled' attribute of the element or of an ancestor <fieldset> is
      * updated
      * @param {Boolean} disabled 
      */
-     formDisabledCallback(disabled) {
+    formDisabledCallback(disabled) {
         this.Select.disabled = disabled
     }
 
@@ -87,7 +120,7 @@ class RWC_Select extends HTMLElement {
         this.defaultSelectSize = '4rem'
         this.defaultSelectColor = { h: 207, s: 90, l: 77 }
         this.css = `
-            *,*::before,*::after{margin:0;padding:0}div.select-container{--select-size: [[select_size]];--hue-white: 0, 0%;--white: hsl(var(--hue-white), 100%);--grey-1: hsl(var(--hue-white), 87%);--grey-2: hsl(var(--hue-white), 50%);--color-1a: [[color_1a]];--color-1b: [[color_1b]];--color-2a: [[color_2a]];--color-2b: [[color_2b]];--color-3a: [[color_3a]];position:relative;width:100%;color-scheme:dark;font-size:var(--select-size);display:-webkit-inline-box;display:-ms-inline-flexbox;display:inline-flex;-webkit-box-sizing:border-box;box-sizing:border-box}div.select-container *,div.select-container *::before,div.select-container *::after{-webkit-box-sizing:inherit;box-sizing:inherit}div.select-container select{position:relative;height:2.5em;display:block;width:100%;background-color:var(--color-2a);border:none;border-bottom:0.0625em solid var(--grey-1);color:var(--grey-1);font-size:.4em;font-family:inherit;padding:0 .3125em;-webkit-tap-highlight-color:transparent;-webkit-transition:background-color .2s;transition:background-color .2s;cursor:pointer}div.select-container select:hover{background-color:var(--color-2b)}div.select-container select:hover ~ .hover{opacity:1}div.select-container select:focus{outline:none}div.select-container select:focus+.label-text{color:var(--color-1a);opacity:1}div.select-container select:focus ~ .focus{-webkit-transform:scaleX(1);transform:scaleX(1)}div.select-container select:disabled,div.select-container select:disabled+.label-text{color:var(--grey-2);cursor:default;pointer-events:none}div.select-container select optgroup,div.select-container select option{background-color:var(--color-3a)}div.select-container select option:hover,div.select-container select option:checked{background-color:var(--color-1b)}div.select-container select[multiple]{height:unset;min-height:6.25em;outline:.125em solid transparent;border:0.0625em solid var(--grey-2);-webkit-transition:outline 300ms cubic-bezier(0, 0, 0.2, 1) 0ms;transition:outline 300ms cubic-bezier(0, 0, 0.2, 1) 0ms}div.select-container select[multiple]:hover{outline-color:var(--white)}div.select-container select[multiple]:focus{outline-color:var(--color-1b)}div.select-container select[multiple] ~ .hover,div.select-container select[multiple] ~ .focus{display:none}div.select-container select[multiple]:disabled optgroup,div.select-container select[multiple]:disabled option{background-color:unset}div.select-container select[multiple] option:disabled:hover{background-color:unset}div.select-container .label-text{position:absolute;top:0;left:.625em;font-size:.4em;opacity:.7;line-height:2.5em;pointer-events:none;-webkit-transform-origin:top left;transform-origin:top left;-webkit-transform:translateY(-1.5em) scale(0.75);transform:translateY(-1.5em) scale(0.75)}div.select-container .hover{position:absolute;bottom:0;left:0;width:100%;height:.05em;background-color:var(--white);opacity:0;-webkit-transition:opacity .2s;transition:opacity .2s;pointer-events:none}div.select-container .focus{position:absolute;left:0;right:0;bottom:0;height:.05em;background-color:var(--color-1b);-webkit-transform:scaleX(0);transform:scaleX(0);-webkit-transition:300ms cubic-bezier(0, 0, 0.2, 1) 0ms;transition:300ms cubic-bezier(0, 0, 0.2, 1) 0ms;pointer-events:none}
+            *,*::before,*::after{margin:0;padding:0}div.select-container{--select-size: [[select_size]];--hue-white: 0, 0%;--white: hsl(var(--hue-white), 100%);--grey-1: hsl(var(--hue-white), 87%);--grey-2: hsl(var(--hue-white), 50%);--color-1a: [[color_1a]];--color-1b: [[color_1b]];--color-2a: [[color_2a]];--color-2b: [[color_2b]];--color-3a: [[color_3a]];position:relative;width:100%;color-scheme:dark;font-size:var(--select-size);display:-webkit-inline-box;display:-ms-inline-flexbox;display:inline-flex;-webkit-box-sizing:border-box;box-sizing:border-box}div.select-container *,div.select-container *::before,div.select-container *::after{-webkit-box-sizing:inherit;box-sizing:inherit}div.select-container select{position:relative;height:2.5em;display:block;width:100%;background-color:var(--color-2a);border:none;border-bottom:0.0625em solid var(--grey-1);color:var(--grey-1);font-size:.4em;font-family:inherit;padding:0 .3125em;-webkit-tap-highlight-color:transparent;-webkit-transition:background-color .2s;transition:background-color .2s;cursor:pointer}div.select-container select:hover{background-color:var(--color-2b)}div.select-container select:hover ~ .hover{opacity:1}div.select-container select:focus{outline:none}div.select-container select:focus+.label-text{color:var(--color-1a);opacity:1}div.select-container select:focus ~ .focus{-webkit-transform:scaleX(1);transform:scaleX(1)}div.select-container select:disabled,div.select-container select:disabled+.label-text{color:var(--grey-2);cursor:default;pointer-events:none}div.select-container select optgroup,div.select-container select option{background-color:var(--color-3a)}div.select-container select option:hover,div.select-container select option:checked{background-color:var(--color-1b)}div.select-container select[multiple]{height:unset;min-height:6.25em;padding:unset;outline:.125em solid transparent;border:0.0625em solid var(--grey-2);-webkit-transition:outline 300ms cubic-bezier(0, 0, 0.2, 1) 0ms;transition:outline 300ms cubic-bezier(0, 0, 0.2, 1) 0ms}div.select-container select[multiple]:hover{outline-color:var(--white)}div.select-container select[multiple]:focus{outline-color:var(--color-1b)}div.select-container select[multiple] ~ .hover,div.select-container select[multiple] ~ .focus{display:none}div.select-container select[multiple]:disabled optgroup,div.select-container select[multiple]:disabled option{background-color:unset}div.select-container select[multiple] option{padding:0 .3125em}div.select-container select[multiple] option:disabled:hover{background-color:unset}div.select-container select[multiple] option:disabled:checked{background-color:unset}div.select-container .label-text{position:absolute;top:0;left:.625em;font-size:.4em;opacity:.7;line-height:2.5em;pointer-events:none;-webkit-transform-origin:top left;transform-origin:top left;-webkit-transform:translateY(-1.5em) scale(0.75);transform:translateY(-1.5em) scale(0.75)}div.select-container .hover{position:absolute;bottom:0;left:0;width:100%;height:.05em;background-color:var(--white);opacity:0;-webkit-transition:opacity .2s;transition:opacity .2s;pointer-events:none}div.select-container .focus{position:absolute;left:0;right:0;bottom:0;height:.05em;background-color:var(--color-1b);-webkit-transform:scaleX(0);transform:scaleX(0);-webkit-transition:300ms cubic-bezier(0, 0, 0.2, 1) 0ms;transition:300ms cubic-bezier(0, 0, 0.2, 1) 0ms;pointer-events:none}
         `
 
         const template = document.createElement('template')
@@ -182,19 +215,40 @@ class RWC_Select extends HTMLElement {
             .replace('[[color_3a]]', color3a)
     }
     
-    onDOMSubtreeModified() {
+    DOMSubtreeModified() {
         this.Select.innerHTML = null
         for (const child of this.children) this.Select.appendChild(child.cloneNode(true))
         
         this.setFormValue()
         this.validation()
     }
+
+    /**
+     * Updates this.Select.innerHTML if this.innerHTML is updated
+     * @param {MutationRecord[]} MutationRecords The list of observed mutations
+     */
+    mutationCallback(MutationRecords) {
+        const targets = []
+
+        for (const record of MutationRecords) {
+            if (record.type.toLowerCase() === 'attributes') targets.push(record.target)
+        }
+
+        // If all changes were on the attributes of rwc-select, ignore them
+        if (targets.every(target => target === this)) return
+        
+        this.DOMSubtreeModified()
+    }
     
     connectedCallback() {
-        this.onDOMSubtreeModified()
+        this.DOMSubtreeModified()
         this.updateSizeColor()
         
-        this.addEventListener('DOMSubtreeModified', () => this.onDOMSubtreeModified())
+        const boundMutationCallback = this.mutationCallback.bind(this)
+        this.mutationObserver = new MutationObserver(boundMutationCallback)
+        this.mutationObserver.observe(this, {attributes: true, childList: true, subtree: true})
+
+        
         this.Select.addEventListener('change', () => {
             this.setFormValue()
             this.validation()
@@ -240,5 +294,3 @@ class RWC_Select extends HTMLElement {
 }
 
 window.customElements.define('rwc-select', RWC_Select)
-
-// TODO handle submitting multiple values
