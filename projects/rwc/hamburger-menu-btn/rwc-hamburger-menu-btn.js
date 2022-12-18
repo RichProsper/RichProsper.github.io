0edbbc9b68b393ace49fa9c819810f91a3be7a62
Rich Prosper <47926284+RichProsper@users.noreply.github.com>
2022-12-18 19:49:34 -0400

    Combined to form one button

--- projects/rwc/hamburger-menu-btn/rwc-hamburger-menu-btn.js
+++ projects/rwc/hamburger-menu-btn/rwc-hamburger-menu-btn.js
@@ -1,7 +1,7 @@
-class RWC_HamburgerMenuBtn1 extends HTMLElement {
+class RWC_HamburgerMenuBtn extends HTMLElement {
     static get observedAttributes() {
         return [
-            'aria-controls', 'title', 'background-color', 'color', 'border-color', 'line-color', 'line-top-color', 'line-middle-color', 'line-bottom-color', 'background-color-expanded', 'color-expanded', 'border-color-expanded', 'line-color-expanded', 'line-top-color-expanded', 'line-bottom-color-expanded', 'size'
+            'aria-controls', 'title', 'background-color', 'color', 'border-color', 'line-color', 'line-top-color', 'line-middle-color', 'line-bottom-color', 'background-color-expanded', 'color-expanded', 'border-color-expanded', 'line-color-expanded', 'line-top-color-expanded', 'line-bottom-color-expanded', 'size', 'animation-style'
         ]
     }
 
@@ -80,6 +80,11 @@ class RWC_HamburgerMenuBtn1 extends HTMLElement {
 
     get size()  { return this.getAttribute('size') || '' }
     set size(s) { s ? this.setAttribute('size', s) : this.removeAttribute('size') }
+
+    get animationStyle()   { return this.getAttribute('animation-style') || '' }
+    set animationStyle(aS) {
+        aS ? this.setAttribute('animation-style', aS) : this.removeAttribute('animation-style')
+    }
     // #endregion
 
     constructor() {
@@ -93,6 +98,8 @@ class RWC_HamburgerMenuBtn1 extends HTMLElement {
 
         this.HamburgerMenuBtn = this.shadowRoot.querySelector('.hamburger-menu-btn')
         this.Style = this.shadowRoot.querySelector('style')
+        this.SVG1 = this.shadowRoot.querySelector('.style-1')
+        this.SVG2 = this.shadowRoot.querySelector('.style-2')
     }
 
     getTemplate() {
@@ -102,17 +109,22 @@ class RWC_HamburgerMenuBtn1 extends HTMLElement {
         this.defaultColor = 'hsl(0 0% 87%)'
         this.defaultSize = '4rem'
         this.css = `
-            *,*::before,*::after{margin:0;padding:0}.hamburger-menu-btn{--background-color: [[background-color]];--border-color: [[border-color]];--line-top-color: [[line-top-color]];--line-middle-color: [[line-middle-color]];--line-bottom-color: [[line-bottom-color]];--background-color-expanded: [[background-color-expanded]];--border-color-expanded: [[border-color-expanded]];--line-top-color-expanded: [[line-top-color-expanded]];--line-bottom-color-expanded: [[line-bottom-color-expanded]];--size: [[size]];box-sizing:border-box;display:flex;width:1em;height:1em;font-size:var(--size);border:none;background:none;background-color:var(--background-color);border-radius:.1em;border:.05em solid var(--border-color);padding:0 .1em;cursor:pointer;transition:outline-offset .4s,opacity .4s,background-color .4s,border-color .4s}.hamburger-menu-btn:hover{opacity:.75}.hamburger-menu-btn:focus{outline:-webkit-focus-ring-color auto 1px;outline-offset:.1em}.hamburger-menu-btn *,.hamburger-menu-btn *::before,.hamburger-menu-btn *::after{box-sizing:inherit}.hamburger-menu-btn svg{width:100%;height:100%;transition:rotate .2s ease-in .2s}.hamburger-menu-btn svg .line{transform-origin:center;transition:rotate .2s ease-in,y .2s ease-in .2s,opacity .1s ease-in .2s,fill .4s}.hamburger-menu-btn svg .line.top{fill:var(--line-top-color)}.hamburger-menu-btn svg .line.middle{fill:var(--line-middle-color)}.hamburger-menu-btn svg .line.bottom{fill:var(--line-bottom-color)}.hamburger-menu-btn[aria-expanded=true]{background-color:var(--background-color-expanded);border-color:var(--border-color-expanded)}.hamburger-menu-btn[aria-expanded=true] svg{rotate:.25turn}.hamburger-menu-btn[aria-expanded=true] svg .line{transition:y .2s ease-in,rotate .2s ease-in .2s,opacity .1s ease-in .2s,fill .4s}.hamburger-menu-btn[aria-expanded=true] svg .line:is(.top,.bottom){y:45}.hamburger-menu-btn[aria-expanded=true] svg .line.top{rotate:45deg;fill:var(--line-top-color-expanded)}.hamburger-menu-btn[aria-expanded=true] svg .line.middle{opacity:0}.hamburger-menu-btn[aria-expanded=true] svg .line.bottom{rotate:-45deg;fill:var(--line-bottom-color-expanded)}
+            *,*::before,*::after{margin:0;padding:0}.hamburger-menu-btn{--background-color: [[background-color]];--border-color: [[border-color]];--line-top-color: [[line-top-color]];--line-middle-color: [[line-middle-color]];--line-bottom-color: [[line-bottom-color]];--background-color-expanded: [[background-color-expanded]];--border-color-expanded: [[border-color-expanded]];--line-top-color-expanded: [[line-top-color-expanded]];--line-bottom-color-expanded: [[line-bottom-color-expanded]];--size: [[size]];box-sizing:border-box;display:flex;width:1em;height:1em;font-size:var(--size);border:none;background:none;background-color:var(--background-color);border-radius:.1em;border:.05em solid var(--border-color);padding:0 .1em;cursor:pointer;transition:opacity .2s,background-color .4s,border-color .4s}.hamburger-menu-btn:hover,.hamburger-menu-btn:focus{opacity:.65}.hamburger-menu-btn *,.hamburger-menu-btn *::before,.hamburger-menu-btn *::after{box-sizing:inherit}.hamburger-menu-btn[data-svg="2"]{transition:opacity .2s,background-color .5s,border-color .5s}.hamburger-menu-btn svg{width:100%;height:100%;transition:rotate .2s ease-in .2s}.hamburger-menu-btn svg.hidden{display:none}.hamburger-menu-btn svg.style-1 .line{transform-origin:center;transition:rotate .2s ease-in,y .2s ease-in .2s,opacity .1s ease-in .2s,fill .4s}.hamburger-menu-btn svg.style-1 .line.top{fill:var(--line-top-color)}.hamburger-menu-btn svg.style-1 .line.middle{fill:var(--line-middle-color)}.hamburger-menu-btn svg.style-1 .line.bottom{fill:var(--line-bottom-color)}.hamburger-menu-btn svg.style-2 .line{stroke-width:10;stroke-linecap:round;stroke-dasharray:80;stroke-dashoffset:0;transform-origin:center;transition:opacity .2s ease-in .2s,stroke .5s}.hamburger-menu-btn svg.style-2 .line.top{--rotation: 45deg;--translate: -20px 20px;stroke:var(--line-top-color)}.hamburger-menu-btn svg.style-2 .line.middle{stroke:var(--line-middle-color)}.hamburger-menu-btn svg.style-2 .line.bottom{--rotation: -45deg;--translate: -20px -20px;stroke:var(--line-bottom-color)}.hamburger-menu-btn svg.style-2.closed .line:is(.top,.bottom){-webkit-animation:open-icon .5s forwards;animation:open-icon .5s forwards}.hamburger-menu-btn[aria-expanded=true]{background-color:var(--background-color-expanded);border-color:var(--border-color-expanded)}.hamburger-menu-btn[aria-expanded=true] svg{rotate:.25turn}.hamburger-menu-btn[aria-expanded=true] svg.style-1 .line{transition:y .2s ease-in,rotate .2s ease-in .2s,opacity .1s ease-in .2s,fill .4s}.hamburger-menu-btn[aria-expanded=true] svg.style-1 .line:is(.top,.bottom){y:45}.hamburger-menu-btn[aria-expanded=true] svg.style-1 .line.top{rotate:.125turn;fill:var(--line-top-color-expanded)}.hamburger-menu-btn[aria-expanded=true] svg.style-1 .line.middle{opacity:0}.hamburger-menu-btn[aria-expanded=true] svg.style-1 .line.bottom{rotate:-0.125turn;fill:var(--line-bottom-color-expanded)}.hamburger-menu-btn[aria-expanded=true] svg.style-2 .line{transition:opacity .2s ease-in .2s,stroke .5s}.hamburger-menu-btn[aria-expanded=true] svg.style-2 .line:is(.top,.bottom){-webkit-animation:close-icon .5s forwards;animation:close-icon .5s forwards}.hamburger-menu-btn[aria-expanded=true] svg.style-2 .line.top{stroke:var(--line-top-color-expanded)}.hamburger-menu-btn[aria-expanded=true] svg.style-2 .line.middle{opacity:0}.hamburger-menu-btn[aria-expanded=true] svg.style-2 .line.bottom{stroke:var(--line-bottom-color-expanded)}@-webkit-keyframes open-icon{0%{stroke-dashoffset:0;translate:var(--translate);rotate:var(--rotation)}40%{stroke-dashoffset:79.9;translate:var(--translate);rotate:var(--rotation)}60%{stroke-dashoffset:79.9}100%{stroke-dashoffset:0}}@keyframes open-icon{0%{stroke-dashoffset:0;translate:var(--translate);rotate:var(--rotation)}40%{stroke-dashoffset:79.9;translate:var(--translate);rotate:var(--rotation)}60%{stroke-dashoffset:79.9}100%{stroke-dashoffset:0}}@-webkit-keyframes close-icon{0%{stroke-dashoffset:0}40%{stroke-dashoffset:79.9}60%{stroke-dashoffset:79.9;translate:var(--translate);rotate:var(--rotation)}100%{stroke-dashoffset:0;translate:var(--translate);rotate:var(--rotation)}}@keyframes close-icon{0%{stroke-dashoffset:0}40%{stroke-dashoffset:79.9}60%{stroke-dashoffset:79.9;translate:var(--translate);rotate:var(--rotation)}100%{stroke-dashoffset:0;translate:var(--translate);rotate:var(--rotation)}}
         `
         const template = document.createElement('template')
         template.innerHTML = `
             <style>${this.css}</style>
 
             <button type="button" class="hamburger-menu-btn" aria-expanded="false">
-                <svg viewBox="0 0 100 100" width="250">
+                <svg class="style-1" viewBox="0 0 100 100" width="250">
                     <rect class="line top" width="80" height="10" x="10" y="17.5" rx="5"></rect>
                     <rect class="line middle" width="80" height="10" x="10" y="45" rx="5"></rect>
                     <rect class="line bottom" width="80" height="10" x="10" y="72.5" rx="5"></rect>
+                </svg>
+                <svg class="style-2 hidden" viewBox="0 0 100 100" width="250">
+                    <line class="line top" x1="90" x2="10" y1="22.5" y2="22.5"></line>
+                    <line class="line middle" x1="10" x2="90" y1="50" y2="50"></line>
+                    <line class="line bottom"  x1="10" x2="90" y1="77.5" y2="77.5"></line>
                 </svg>
             </button>
         `
@@ -154,8 +166,15 @@ class RWC_HamburgerMenuBtn1 extends HTMLElement {
         if (!this.hasAttribute('title')) this.HamburgerMenuBtn.ariaLabel = this.defaultTitle
         if (!this.hasAttribute('title')) this.HamburgerMenuBtn.title = this.defaultTitle
 
+        const SVG2 = this.SVG2
         this.HamburgerMenuBtn.addEventListener('click', function() {
-            this.ariaExpanded === 'true' ? this.ariaExpanded = 'false' : this.ariaExpanded = 'true'
+            this.ariaExpanded === 'true' ? (
+                this.ariaExpanded = 'false',
+                SVG2.classList.add('closed')
+            ) : (
+                this.ariaExpanded = 'true',
+                SVG2.classList.remove('closed')
+            )
         })
 
         this.updateStyles()
@@ -199,9 +218,24 @@ class RWC_HamburgerMenuBtn1 extends HTMLElement {
                 this.updateStyles()
                 break
             }
+            case 'animation-style': {
+                (newValue  && newValue === '2') ? (
+                    this.SVG1.classList.add('hidden'),
+                    this.SVG2.classList.remove('hidden'),
+                    // Stops the closed animation from occurring
+                    this.SVG2.classList.remove('closed'),
+                    this.HamburgerMenuBtn.setAttribute('data-svg', '2')
+
+                ) : (
+                    this.SVG1.classList.remove('hidden'),
+                    this.SVG2.classList.add('hidden'),
+                    this.HamburgerMenuBtn.removeAttribute('data-svg')
+                )
+                break
+            }
             default: {}
         }
     }
 }
 
-window.customElements.define('rwc-hmb-1', RWC_HamburgerMenuBtn1)
+window.customElements.define('rwc-hmb', RWC_HamburgerMenuBtn)
