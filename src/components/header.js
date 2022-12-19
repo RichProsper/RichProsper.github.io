8319625f327253a122a63027f1f90e2e49c30b86
Rich Prosper <47926284+RichProsper@users.noreply.github.com>
2022-12-19 00:30:27 -0400

    Minor fix to the hamburger menu

--- src/components/header.js
+++ src/components/header.js
@@ -26,6 +26,7 @@ export default () => {
     const lis = header.children[1].children
     for (let i = 0; i < lis.length; i++) {
         lis[i].children[0].addEventListener('click', toggle)
+        lis[i].children[0].addEventListener('click', () => menu.close())
     }
     menu.addEventListener('click', toggle)
 }
