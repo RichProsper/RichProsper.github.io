da763273279da6268def50b36429e983832fe1a8
Rich Prosper <47926284+RichProsper@users.noreply.github.com>
2022-12-24 18:29:30 -0400

    Adding a theme switcher (2)

--- portfolio/tindog/scripts/navbar--theme-switcher.js
+++ portfolio/tindog/scripts/navbar--theme-switcher.js
@@ -2,6 +2,7 @@ export default () => {
     const li = document.querySelector('nav.navbar li.theme-switcher')
     const atag = li.querySelector('a')
     const form = li.querySelector('form')
+    const themes = form.querySelectorAll('input')
 
     /**
      * Close theme switcher dropdown
@@ -25,4 +26,18 @@ export default () => {
             window.removeEventListener('click', closeThemeSwitcher)
         )
     })
+
+    const activeTheme = localStorage.getItem('theme')
+    if (activeTheme) {
+        form.querySelector(`input#${activeTheme}`).checked = true
+        // The <html> element
+        document.documentElement.className = activeTheme
+    }
+
+    for (const theme of themes) {
+        theme.addEventListener('click', () => {
+            localStorage.setItem('theme', theme.id)
+            document.documentElement.className = theme.id
+        })
+    }
 }
