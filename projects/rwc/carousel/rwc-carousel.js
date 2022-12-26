b1e7fc7bd84dbe2acf9966e535a5f1efff3c04d5
Rich Prosper <47926284+RichProsper@users.noreply.github.com>
2022-12-25 22:01:12 -0400

    Creating a carousel (1)

---
+++ projects/rwc/carousel/rwc-carousel.js
@@ -0,0 +1,85 @@
+class RWC_Carousel extends HTMLElement {
+    // TODO
+    static get observedAttributes() {
+        return []
+    }
+
+    // #region Getters & Setters
+    // #endregion
+
+    constructor() {
+        super()
+        this.init()
+    }
+
+    init() {
+        this.attachShadow({mode: 'open'})
+        this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true))
+
+        this.Carousel = this.shadowRoot.querySelector('.carousel')
+        this.Style = this.shadowRoot.querySelector('style')
+    }
+
+    // TODO
+    getTemplate() {
+        // this.defaultAttribute = ''
+        this.css = ``
+        const template = document.createElement('template')
+        template.innerHTML = `
+            <link rel="stylesheet" href="style.min.css">
+            <style>${this.css}</style>
+
+            <div class="carousel">
+                <div class="items">
+                    <slot name="item"></slot>
+                </div>
+
+                <div class="numbers">
+                    <span class="current">1</span>/<span class="total">1</span>
+                </div>
+
+                <button type="button" class="prev" title="Previous">
+                    <svg class="left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                        <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                        <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 278.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/>
+                    </svg>
+                    <svg class="down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                        <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                        <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/>
+                    </svg>
+                </button>
+
+                <button type="button" class="next" title="Next">
+                    <svg class="right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                        <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                        <path d="M342.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L274.7 256 105.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/>
+                    </svg>
+                    <svg class="up" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                        <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                        <path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"/>
+                    </svg>
+                </button>
+            </div>
+        `
+
+        return template
+    }
+
+    // TODO
+    connectedCallback() {
+        
+    }
+
+    // TODO
+    // This life cycle hook is ran before connectedCallback()
+    attributeChangedCallback(name, oldValue, newValue) {
+        switch (name) {
+            case '': {
+                break
+            }
+            default: {}
+        }
+    }
+}
+
+window.customElements.define('rwc-carousel', RWC_Carousel)
